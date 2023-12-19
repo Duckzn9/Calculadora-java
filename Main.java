119413a0242c096cedaf78fd707409b6b1333360
Bezalyel Silva <duckrei09@gmail.com>
2023-12-19 18:49:02 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,8 @@
+//Calculadora by duck
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int resultado = 0;
-        int numero = 0;
         int soma, subtrair, multiplicar, dividir;
 
         System.out.println("Bem vindo a Calculadora de Duck");
