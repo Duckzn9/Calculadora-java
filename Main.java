97b657ad40a53bc93b2a9f154501c04258a498bb
Bezalyel Silva <duckrei09@gmail.com>
2023-12-28 20:18:38 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,34 +1,35 @@
-//Calculadora by duck
+// Calculator by Duck
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int soma, subtrair, multiplicar, dividir;
+        int sum, subtract, multiply, divide;
 
-        System.out.println("Bem vindo a Calculadora de Duck");
-        System.out.println("Digite o primeiro Numero");
+        System.out.println("Welcome to Duck's Calculator");
+        System.out.println("Enter the first number:");
         int number_1 = scanner.nextInt();
-        System.out.println("Digite o segundo Numero: ");
+        System.out.println("Enter the second number:");
         int number_2 = scanner.nextInt();
         
-        System.out.println("Escolha uma operaçao: \n 1. Somar \n 2. Subtrair \n 3. Multiplicar \n 4. Dividir");
-        int operacao = scanner.nextInt();
+        System.out.println("Choose an operation: \n 1. Add \n 2. Subtract \n 3. Multiply \n 4. Divide");
+        int operation = scanner.nextInt();
         
-        if (operacao == 1) {
-            soma = number_1 + number_2;
-            System.out.printf("%d + %d = %d", number_1, number_2, soma);
-        } else if (operacao == 2) {
-            subtrair = number_1 - number_2;
-            System.out.printf("%d - %d = %d", number_1, number_2, subtrair);
-        } else if (operacao == 3) {
-            multiplicar = number_1 * number_2;
-            System.out.printf("%d * %d = %d", number_1, number_2, multiplicar);
-        } else if (operacao == 4) {
-            dividir = number_1 / number_2;
-            System.out.printf("%d / %d = %d", number_1, number_2, dividir);
+        if (operation == 1) {
+            sum = number_1 + number_2;
+            System.out.printf("%d + %d = %d", number_1, number_2, sum);
+        } else if (operation == 2) {
+            subtract = number_1 - number_2;
+            System.out.printf("%d - %d = %d", number_1, number_2, subtract);
+        } else if (operation == 3) {
+            multiply = number_1 * number_2;
+            System.out.printf("%d * %d = %d", number_1, number_2, multiply);
+        } else if (operation == 4) {
+            divide = number_1 / number_2;
+            System.out.printf("%d / %d = %d", number_1, number_2, divide);
         } else {
-            System.out.println("Numero Ivalido");
-            System.out.println("Tente usar um Numero da Tabela");
+            System.out.println("Invalid Number");
+            System.out.println("Try using a number from the table");
         }
     }
 }
